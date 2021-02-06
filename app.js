f3b6f86820bc9ea6f401b59149f9b7e35648c423
johnnylysfu <68446215+johnnylysfu@users.noreply.github.com>
2021-02-05 23:36:57 -0800

    Add files via upload
    2/5/2021 
    added app section
    updated help section
    added footer
    updated font sizes

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+window.onscroll = function() {scrollShrinkFunction()};
+
+function scrollShrinkFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementByClassName("navbar").style.padding = "30px 10px";
+        document.getElementById("navbar__logo").style.fontSize = "25px";
+      } else {
+        document.getElementByClassName("navbar").style.padding = "80px 10px";
+        document.getElementById("navbar__logo").style.fontSize = "35px";
+      }
+    }
